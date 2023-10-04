d6357e335f46fb286dfbd9a7d65f6441e6d935bb
Pedro Henrique Alves Cruz <phac@cin.ufpe.br>
2023-10-04 20:13:00 -0300

    Classes renamed and Internationalization added changes: - Added US english language - Added BR portuguese language - Renamed the Hello classes to Main

--- src/main/java/com/example/launcherfx2/MainApplication.java
+++ src/main/java/com/example/launcherfx2/MainApplication.java
@@ -6,11 +6,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Locale currentLocale = Locale.getDefault();
+        Locale brazilianPortuguese = new Locale("pt_BR");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main.fxml"));
+        fxmlLoader.setResources(ResourceBundle.getBundle("general", brazilianPortuguese));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
